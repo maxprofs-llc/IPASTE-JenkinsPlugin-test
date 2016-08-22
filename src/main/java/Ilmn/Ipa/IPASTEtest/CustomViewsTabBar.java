05cb1b0c4fe812b3c605f1e3c5d25833d31df48e
Wataf <tyhuber2@gmail.com>
2016-08-21 19:10:00 -0700

    Initial commit

---
+++ src/main/java/Ilmn/Ipa/IPASTEtest/CustomViewsTabBar.java
@@ -0,0 +1,29 @@
+/*package Ilmn.Ipa.IPASTEtest;
+
+
+public class CustomViewsTabBar extends ViewsTabBar {
+
+    @DataBoundConstructor
+    public CustomViewsTabBar() {
+        super();
+    }
+
+    @Extension
+    public static final class CustomViewsTabBarDescriptor extends ViewsTabBarDescriptor {
+
+        public CustomViewsTabBarDescriptor() {
+            load();
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Custom Views TabBar";
+        }
+
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
+            save();
+            return false;
+        }
+    }
+}*/
---
+++ src/main/java/Ilmn/Ipa/IPASTEtest/HelloWorldBuilder.java
@@ -0,0 +1,162 @@
+package Ilmn.Ipa.IPASTEtest;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.model.AbstractProject;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.tasks.BatchFile;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.Builder;
+import hudson.util.FormValidation;
+import jenkins.tasks.SimpleBuildStep;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.StaplerRequest;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+/**
+ * Sample {@link Builder}.
+ *
+ * <p>
+ * When the user configures the project and enables this builder,
+ * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
+ * and a new {@link HelloWorldBuilder} is created. The created
+ * instance is persisted to the project configuration XML by using
+ * XStream, so this allows you to use instance fields (like {@link #command})
+ * to remember the configuration.
+ *
+ * <p>
+ * When a build is performed, the {@link #perform} method will be invoked.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
+
+    private final String command;
+
+    // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
+    @DataBoundConstructor
+    public HelloWorldBuilder(String command) {
+        this.command = command;
+    }
+
+    /**
+     * We'll use this from the <tt>config.jelly</tt>.
+     */
+    public String getCommand() {
+        return command;
+    }
+
+    @Override
+    public void perform(Run<?,?> build, FilePath workspace, Launcher launcher, TaskListener listener) throws IOException, InterruptedException {
+        // This is where you 'build' the project.
+        // Since this is a dummy, we just say 'hello world' and call that a build.
+
+        // This also shows how you can consult the global configuration of the builder
+
+            listener.getLogger().println("Running command "+ command +"!");
+            FilePath file = new FilePath(workspace,"Test.txt");
+            file.write("Test","UTF-8");
+
+            BatchFile batchFile = new BatchFile("echo Running script\r\n" +
+                    command);
+            FilePath tmpFile = batchFile.createScriptFile(workspace);
+            int exit = launcher.launch().cmds(batchFile.buildCommandLine(tmpFile)).stdout(listener).pwd(workspace).join();
+            listener.getLogger().println("Ran command. Got exit code " + exit);
+    }
+
+    // Overridden for better type safety.
+    // If your plugin doesn't really define any property on Descriptor,
+    // you don't have to do this.
+    @Override
+    public DescriptorImpl getDescriptor() {
+        return (DescriptorImpl)super.getDescriptor();
+    }
+
+    /**
+     * Descriptor for {@link HelloWorldBuilder}. Used as a singleton.
+     * The class is marked as public so that it can be accessed from views.
+     *
+     * <p>
+     * See <tt>src/main/resources/hudson/plugins/hello_world/HelloWorldBuilder/*.jelly</tt>
+     * for the actual HTML fragment for the configuration screen.
+     */
+    @Extension // This indicates to Jenkins that this is an implementation of an extension point.
+    public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+        /**
+         * To persist global configuration information,
+         * simply store it in a field and call save().
+         *
+         * <p>
+         * If you don't want fields to be persisted, use <tt>transient</tt>.
+         */
+        private boolean useFrench;
+
+        /**
+         * In order to load the persisted global configuration, you have to 
+         * call load() in the constructor.
+         */
+        public DescriptorImpl() {
+            load();
+        }
+
+        /**
+         * Performs on-the-fly validation of the form field 'command'.
+         *
+         * @param value
+         *      This parameter receives the value that the user has typed.
+         * @return
+         *      Indicates the outcome of the validation. This is sent to the browser.
+         *      <p>
+         *      Note that returning {@link FormValidation#error(String)} does not
+         *      prevent the form from being saved. It just means that a message
+         *      will be displayed to the user. 
+         */
+        public FormValidation doCheckName(@QueryParameter String value)
+                throws IOException, ServletException {
+            if (value.length() == 0)
+                return FormValidation.error("Please set a command");
+            if (value.length() < 4)
+                return FormValidation.warning("Isn't the command too short?");
+            return FormValidation.ok();
+        }
+
+        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+            // Indicates that this builder can be used with all kinds of project types 
+            return true;
+        }
+
+        /**
+         * This human readable command is used in the configuration screen.
+         */
+        public String getDisplayName() {
+            return "Input command to run";
+        }
+
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
+            // To persist global configuration information,
+            // set that to properties and call save().
+            useFrench = formData.getBoolean("useFrench");
+            // ^Can also use req.bindJSON(this, formData);
+            //  (easier when there are many fields; need set* methods for this, like setUseFrench)
+            save();
+            return super.configure(req,formData);
+        }
+
+        /**
+         * This method returns true if the global configuration says we should speak French.
+         *
+         * The method command is bit awkward because global.jelly calls this method to determine
+         * the initial state of the checkbox by the naming convention.
+         */
+        public boolean getUseFrench() {
+            return useFrench;
+        }
+    }
+}
+
